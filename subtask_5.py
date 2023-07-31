c0fc63ad9ca0c0dc62512ec18978965fef0a3f05
Alpine-Coder <jul.hinderer@gmail.com>
2023-07-31 09:17:36 +0200

    Added heatmap with Google Maps overlay to subtask_5

--- subtask_5.py
+++ subtask_5.py
@@ -131,3 +131,90 @@ if __name__ == "__main__":
     heatmap_filename = "heatmap.png"
     plt.savefig(heatmap_filename, dpi=300, bbox_inches="tight")
     plt.close()
+
+
+    ############################# Second approach (Google Maps overlay) #############################
+
+    # ppb means for every station
+    ppb_means = [avg_STA, avg_LHA, avg_LOT, avg_JOH, avg_ALL]
+    print(ppb_means)
+
+    # Initialize 20 x 20 grid
+    grid_size = 20
+    grid = np.zeros((grid_size, grid_size))
+
+    # Positions of the stations in the grid
+    station_coords = [(8, 8), (6, 10), (8, 10), (15, 12), (1, 13)]
+
+    def euclidean_distance(x1, y1, x2, y2):
+        return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    # Interpolate values using IDW
+    for i in range(grid_size):
+        for j in range(grid_size):
+            x, y = i, j  # Coordinates of the current grid cell
+
+            # Check if the current coordinates match any of the station coordinates
+            station_idx = -1
+            for idx, (station_x, station_y) in enumerate(station_coords):
+                if x == station_x and y == station_y:
+                    station_idx = idx
+                    break
+
+            if station_idx != -1:
+                # Use the actual measured value for this station
+                grid[i, j] = ppb_means[station_idx]
+            else:
+                total_weighted_sum = 0
+                total_weights = 0
+
+                for idx in range(len(ppb_means)):
+                    # Calculate the distance between the current grid cell and the measurement station
+                    distance = euclidean_distance(x, y, station_coords[idx][0], station_coords[idx][1])
+
+                    # Calculate the weight using the inverse distance formula
+                    weight = 1 / distance if distance != 0 else 1
+
+                    # Calculate the weighted sum of the measured concentrations
+                    total_weighted_sum += weight * ppb_means[idx]
+
+                    # Sum the weights for normalization
+                    total_weights += weight
+
+                # Calculate the interpolated value for the current grid cell
+                if total_weights != 0:
+                    grid[i, j] = total_weighted_sum / total_weights
+    
+    # Load the image of the city or region (Replace 'city_map_image.png' with your actual image file)
+    city_map = plt.imread('Munich.png')
+
+    # Calculate the number of grid cells in each dimension
+    num_cells_x, num_cells_y = grid.shape
+
+    # Create the figure and axis
+    plt.figure(figsize=(10, 10))
+
+    # Plot the city map
+    plt.imshow(city_map, extent=[0, num_cells_x, 0, num_cells_y], alpha=0.8)
+
+    # Plot the concentration data on the map using pcolormesh
+    cmap = plt.get_cmap('YlOrRd')  # Colormap: Red-Yellow-Green (low to high concentrations)
+    concentration_plot = plt.pcolormesh(np.arange(num_cells_x+1), np.arange(num_cells_y+1), grid.T, cmap=cmap, shading='auto', alpha=0.7)
+
+    # Add a color bar
+    cbar = plt.colorbar(concentration_plot)
+    cbar.set_label('Concentration (ppb)')
+
+    # Set the aspect ratio to be equal
+    plt.gca().set_aspect('equal')
+
+    # Set the x and y axis ticks to be in steps of 1
+    plt.gca().xaxis.set_major_locator(plt.MultipleLocator(1))
+    plt.gca().yaxis.set_major_locator(plt.MultipleLocator(1))
+
+    # Set the plot title
+    plt.title('NO2 Concentration Map')
+
+    # Save the plot
+    plt.savefig("heatmap_google_maps", dpi=300, bbox_inches="tight")
+    plt.close()
